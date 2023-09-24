486af01f3d28c243f6dce3efcc0231463fdeb427
hehmdalolkek <badeha.inna@yandex.ru>
2023-09-25 00:40:48 +0400

    added sessions

--- app.js
+++ app.js
@@ -2,18 +2,11 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const handlers = require('./lib/handlers');
 const db = require('./lib/db');
 
 const app = express();
-app.use(
-	session({
-		secret: 'key',
-		resave: false,
-		saveUninitialized: false
-	})
-);
-app.use(express.urlencoded({extended: true}));
 app.engine('handlebars', expressHandlebars.engine({
 	defaultLayout: 'main',
 	helpers: {
@@ -29,15 +22,55 @@ app.engine('handlebars', expressHandlebars.engine({
 app.set('view engine', 'handlebars');
 app.disable('x-powered-by');
 
+app.use(express.urlencoded({extended: true}));
+
+
+const optionsDB = {
+	connectionLimit: 5,
+	// host: 'bf8pdofghs5dakuq9dwl-mysql.services.clever-cloud.com',
+	// user: 'uu5f0avbqckl327q',
+	// database: 'bf8pdofghs5dakuq9dwl',
+	// password: '6J1880YEeUxzpytKh5A9'
+	host: process.env.DB_HOST,
+	user: process.env.DB_USERNAME,
+	database: process.env.DB_DBNAME,
+	password: process.env.DB_PASSWORD
+};
+
+const sessionStore = new MySQLStore(optionsDB);
+
+app.use(session({
+	key: 'session_cookie_name',
+	secret: 'session_cookie_secret',
+	store: sessionStore,
+	resave: false,
+	saveUninitialized: false
+}));
+
+const isAuth = (req, res, next) => {
+	if (req.session.isAuth) {
+		next();
+	} else {
+		return res.redirect('/');
+	}
+};
+
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', handlers.index);
 app.post('/', handlers.indexPost);
-app.get('/catalog', db.catalog);
-app.get('/create', db.create);
-app.post('/create', db.createPost);
-app.get('/edit/:id', db.edit);
-app.post('/edit/:id', db.editPost);
-app.post('/delete/:id', db.delete);
+app.get('/catalog', isAuth, db.catalog);
+app.get('/create', isAuth, db.create);
+app.post('/create', isAuth, db.createPost);
+app.get('/edit/:id', isAuth, db.edit);
+app.post('/edit/:id', isAuth, db.editPost);
+app.post('/delete/:id', isAuth, db.delete);
+app.post('/logout', (req, res) => {
+	req.session.destroy(err => {
+		if (err) throw err;
+		return res.redirect('/');
+	});
+});
 app.use(handlers.notFound);
 app.use(handlers.serverError);
 
